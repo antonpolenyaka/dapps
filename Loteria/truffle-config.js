8014217b410ede9f66e932ee174647cc1d0aae8d
Anton P <anton@antonp.com>
2021-12-02 08:28:08 +0100

    configuracion del truffle

--- Loteria/truffle-config.js
+++ Loteria/truffle-config.js
@@ -1,13 +1,18 @@
 require('babel-register');
 require('babel-polyfill');
 
+var mnemonic = "frase semilla";
+var HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   networks: {
+    // Ganche
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     },
+    
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
